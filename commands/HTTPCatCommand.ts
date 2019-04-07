a5d96a8aaf0e7bdb60aff700697d8e8977548948
Thomas Gardner <thomasgardner@orthofix.com>
2019-04-07 14:44:08 -0500

    Update version per request

--- commands/HTTPCatCommand.ts
+++ commands/HTTPCatCommand.ts
@@ -14,7 +14,7 @@ export class HTTPCatCommand implements ISlashCommand {
         const codes = [100, 101, 200, 201, 202, 204, 206, 207, 300, 301, 303, 304, 305, 307, 400, 401, 402, 403, 404,
           405, 406, 408, 409, 410, 411, 413, 414, 416, 417, 418, 422, 423, 424, 425, 426, 429, 431, 444, 450, 500,
           502, 503, 506, 507, 508, 509, 599];
-        const httpCatUrl = 'http://httpcats.herokuapp.com/';
+        const httpCatUrl = 'https://http.cat/';
 
         let result;
         let successful = false; // Way to be pessimistic
